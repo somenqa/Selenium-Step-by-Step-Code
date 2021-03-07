0a49f67ec2ec37bbf250f2db38b21d56aedc77ff
dipika.das <dipika.das@groupm.com>
2021-03-07 11:14:36 -0500

    Selenium 1st practice

---
+++ src/main/java/allbrowserSetup/FirstCodeSetup.java
@@ -0,0 +1,45 @@
+package allbrowserSetup;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.Test;
+
+public class FirstCodeSetup {
+
+    @Test
+    public void SafariBrowser() {
+        WebDriver driver = new SafariDriver();
+        driver.get("http://automationpractice.com/index.php");
+        driver.quit();
+    }
+    @Test
+    public void chromebrowser() {
+        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://automationpractice.com/index.php");
+        driver.quit();
+    }
+    @Test
+    public void Firefoxbrowser() {
+        System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://automationpractice.com/index.php");
+        driver.quit();
+    }
+    @Test
+    public void ChromeBrowserWithWebManager(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver =new ChromeDriver();
+        driver.get("http://automationpractice.com/index.php");
+        driver.quit();
+    }
+
+}
+
+
+
+
+
