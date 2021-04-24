3d8785493e5c960950514ee5dafc8d083a20ab08
dipika.das <dipika.das@groupm.com>
2021-04-23 22:34:54 -0400

    Selenium 1st practice

---
+++ src/main/java/controller/LoginControler.java
@@ -0,0 +1,25 @@
+package controller;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginControler {
+    @FindBy(xpath = "//*[@id='header']/div[2]/div/div/nav/div[1]/a")
+    WebElement signInbutton;
+    @FindBy(id = "email")
+    WebElement inputEmail;
+    @FindBy(id = "passwd")
+    WebElement inputPass;
+    public LoginControler(WebDriver driver){
+        PageFactory.initElements(driver,this);
+    }
+public void LogInValidation(){
+        signInbutton.isDisplayed();
+        signInbutton.click();
+        inputEmail.sendKeys("somen@yahoo.com");
+        inputPass.sendKeys("12345");
+
+    }
+}
